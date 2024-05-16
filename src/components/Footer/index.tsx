96742e5d9e278fc516708fe426a5ed59d74fe5e5
cherrynechou <iboy_2008@163.com>
2024-05-16 13:30:33 +0800

    store model

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,38 +1,43 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-layout';
 
-const { t } = useTranslation();
-
-const currentYear = new Date().getFullYear();
-
-const defaultMessage = t('system.copyright.produced')
-
-const Footer: FC = ()=> (
+const Footer: FC = ()=> {
+  
+  const { t } = useTranslation();
+  
+  const currentYear = new Date().getFullYear();
+  
+  const defaultMessage = t('system.copyright.produced')
+  
+  return (
     <DefaultFooter
-        copyright="2019 蚂蚁金服体验技术部出品"
-        links={[
-            {
-                key: 'Ant Design Pro',
-                title: 'Ant Design Pro',
-                href: 'https://pro.ant.design',
-                blankTarget: true,
-            },
-            {
-                key: 'github',
-                title: <GithubOutlined />,
-                href: 'https://github.com/ant-design/ant-design-pro',
-                blankTarget: true,
-            },
-            {
-                key: 'Ant Design',
-                title: 'Ant Design',
-                href: 'https://ant.design',
-                blankTarget: true,
-            },
-        ]}
+      copyright={ `${currentYear} ${defaultMessage}`  }
+      links={[
+        {
+          key: 'Ant Design Pro',
+          title: 'Ant Design Pro',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'Ant Design',
+          title: 'Ant Design',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
     />
-)
+  )
+  
+}
 
 
 export default Footer;
