a0bce7641678fe6ebe094cbc2e8548bab8b2803d
cherrynechou <iboy_2008@163.com>
2024-05-17 18:15:06 +0800

    add iconfont

---
+++ src/components/IconFont/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FC } from 'react';
+import { createFromIconfontCN } from '@ant-design/icons';
+
+// @ts-ignore
+import { iconFontJS } from '@/assets/iconfont/iconfont.js'
+
+/**
+ * 菜单图标,需要上传到https://www.iconfont.cn/
+ */
+const Icon = createFromIconfontCN({
+  scriptUrl: iconFontJS,
+});
+
+interface IProps {
+  /**
+   * @description 图标样式
+   * */
+  className?: React.CSSProperties;
+  /**
+   * @description IconFont中上传的图标名称
+   * */
+  type?: string;
+  /**
+   * @description 图标样式
+   * */
+  style?: object;
+  /**
+   * @description 图标大小
+   * */
+  size?: string;
+  /**
+   * @description 点击回调
+   * */
+  onClick?: () => void;
+}
+
+const IconFont: FC<IProps> = (props: any) => {
+  
+  const { type, className, style, size = 'inherit', onClick } = props;
+  
+  return (
+    <Icon
+      type={type}
+      className={className}
+      style={{ ...style, fontSize: size }}
+      onClick={onClick}
+    />
+  );
+}
+
+
+export default IconFont;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import { Question, SelectLang } from './RightContent';
 import { AvatarDropdown, AvatarName } from './RightContent/AvatarDropdown';
 import SelectIcon  from './Forms/SelectIcon';
 import CustomerUpload from './Forms/CustomerUpload';
+import IconFont from './IconFont';
 
-export { Footer, Question, SelectLang, AvatarDropdown, AvatarName, SelectIcon, CustomerUpload };
+export { Footer, Question, SelectLang, AvatarDropdown, AvatarName, SelectIcon, CustomerUpload,IconFont };
