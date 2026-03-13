086671af869c5db96ef5d55a425a8128769f2f43
cherrynechou <iboy_2008@163.com>
2026-03-13 21:48:03 +0800

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode })=>{
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the
+  // `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [
@@ -23,7 +26,7 @@ export default defineConfig(({ mode })=>{
     },
     server: {
       host: '0.0.0.0', // 默认是 localhost
-      port: Number(env.VITE_API_PORT) || 8080, // 自定义端口
+      port: env.VITE_API_PORT ? Number(env.VITE_API_PORT) : 8080, // 自定义端口
       strictPort:false, // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
       open: false, // 启动后是否浏览器自动打开
       hmr:true, // 为开发服务启用热更新，默认是不启用热更新的
@@ -32,34 +35,6 @@ export default defineConfig(({ mode })=>{
           target: env.VITE_API_HOST, // easymock
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, "")
-        }
-      }
-    },
-    build: {
-      outDir: 'dist',
-      minify: 'terser',
-      terserOptions:{
-        compress:{
-          drop_console: true,             // Remove console.*
-          drop_debugger: true,           // Remove debugger
-          pure_funcs: ['console.log'], // Remove specific functions
-        },
-        format: {
-          comments: false,         // Remove comments
-        },
-        mangle: {
-          toplevel: true,          // Mangle top-level names
-        }
-      },
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom'],
-            utils: ['lodash', 'axios']
-          },
-          chunkFileNames: 'assets/js/[name]-[hash].js',
-          entryFileNames: 'assets/js/[name]-[hash].js',
-          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
         }
       }
     }
